9994fe60084b6f1fe944890ae170b2231fde5b25
Marvin ROGER <bonjour+git@marvinroger.fr>
2020-03-24 22:49:50 +0100

    fix: update wasm builder to Docker Hub

--- packages/nanocurrency/wasm/build.sh
+++ packages/nanocurrency/wasm/build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-cargo build --target wasm32-unknown-unknown --release
-wasm-snip --snip-rust-fmt-code --snip-rust-panicking-code -o target/wasm32-unknown-unknown/release/nanocurrency_wasm_snip.wasm target/wasm32-unknown-unknown/release/nanocurrency_wasm.wasm
-wasm-opt -O3 -o target/wasm32-unknown-unknown/release/nanocurrency_wasm_snip_opt.wasm target/wasm32-unknown-unknown/release/nanocurrency_wasm_snip.wasm
+# WASM_MODE : debug | release
+
 mkdir -p bin
-cp target/wasm32-unknown-unknown/release/nanocurrency_wasm_snip_opt.wasm bin/nanocurrency.wasm
+
+if [ "$WASM_MODE" == "release" ]; then
+    cargo build --target wasm32-unknown-unknown --release
+    wasm-snip --snip-rust-fmt-code --snip-rust-panicking-code -o target/wasm32-unknown-unknown/release/nanocurrency_wasm_snip.wasm target/wasm32-unknown-unknown/release/nanocurrency_wasm.wasm
+    wasm-opt -O3 -o target/wasm32-unknown-unknown/release/nanocurrency_wasm_snip_opt.wasm target/wasm32-unknown-unknown/release/nanocurrency_wasm_snip.wasm
+    cp target/wasm32-unknown-unknown/release/nanocurrency_wasm_snip_opt.wasm bin/nanocurrency.wasm
+else
+    cargo build --target wasm32-unknown-unknown
+    cp target/wasm32-unknown-unknown/debug/nanocurrency_wasm.wasm bin/nanocurrency.wasm
+fi

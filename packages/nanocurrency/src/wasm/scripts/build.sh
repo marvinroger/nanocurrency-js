f4f24df465d34b177d16d26e2a40115b5632e795
Marvin ROGER <marvinroger@users.noreply.github.com>
2021-05-24 14:34:58 +0200

    chore: fix docker build

--- packages/nanocurrency/src/wasm/scripts/build.sh
+++ packages/nanocurrency/src/wasm/scripts/build.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-mkdir -p lib/
+mkdir -p ./lib/
 
 CFLAGS="-flto -O3 -nostdlib -fno-builtin -ffreestanding --target=wasm32"
 LDFLAGS="-Wl,--strip-all -Wl,--initial-memory=131072 -Wl,--max-memory=131072 -Wl,--no-entry -Wl,--allow-undefined -Wl,--compress-relocations -Wl,--export-dynamic"
@@ -10,4 +10,4 @@ LDFLAGS="-Wl,--strip-all -Wl,--initial-memory=131072 -Wl,--max-memory=131072 -Wl
 # -msimd128 -msign-ext -mmutable-globals -mmultivalue -mbulk-memory -mtail-call -munimplemented-simd128
 # -g -fdebug-prefix-map=/app/src=/C:/Projects/hash-wasm/src
 
-clang ${CFLAGS} ${LDFLAGS} -o /app/lib/nanocurrency.wasm /app/src/main.c
+clang ${CFLAGS} ${LDFLAGS} -o ./lib/nanocurrency.wasm ./src/main.c

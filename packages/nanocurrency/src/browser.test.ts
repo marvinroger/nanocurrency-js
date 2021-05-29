6dbf542ff6b5b94b8462c85040e49e06e40826d6
Marvin ROGER <marvinroger@users.noreply.github.com>
2021-05-29 17:34:04 +0200

    Fix browser test

--- packages/nanocurrency/src/browser.test.ts
+++ packages/nanocurrency/src/browser.test.ts
@@ -21,58 +21,21 @@ describe('browser', () => {
     // load NanoCurrency
     await page.evaluate(umdScript)
 
-    // seed generation
-    const result1 = await page.evaluate(async function() {
+    const generatedSeeds = await page.evaluate(async function() {
       // @ts-expect-error in global browser scope
       const a = await NanoCurrency.generateSeed()
       // @ts-expect-error in global browser scope
       const b = await NanoCurrency.generateSeed()
       return { a, b }
     })
-    expect(result1.a).not.toBe(result1.b)
+    expect(generatedSeeds.a).not.toBe(generatedSeeds.b)
 
-    // webassembly test (test in worker)
-    const result2 = await page.evaluate(
-      function(passed) {
-        return new Promise((resolve, reject) => {
-          const blobURL = URL.createObjectURL(
-            new Blob(
-              [
-                passed.umdScript,
-                '(',
-                function() {
-                  ;((self as unknown) as Worker).postMessage('lol')
-                  // @ts-expect-error in global browser scope
-                  return NanoCurrency.computeWork(
-                    'b9cb6b51b8eb869af085c4c03e7dc539943d0bdde13b21436b687c9c7ea56cb0'
-                  ).then((work: string) => {
-                    ;((self as unknown) as Worker).postMessage(work)
-                    return
-                  })
-                }.toString(),
-                ')()',
-              ],
-              {
-                type: 'application/javascript',
-              }
-            )
-          )
-
-          const worker = new Worker(blobURL)
-          worker.onmessage = function(e) {
-            const work = e.data
-
-            resolve(work)
-          }
-          worker.onerror = function(err) {
-            reject(err)
-          }
-
-          URL.revokeObjectURL(blobURL)
-        })
-      },
-      { umdScript }
-    )
-    expect(result2).toBe('0000000000010600')
+    const foundWork = await page.evaluate(async function() {
+      // @ts-expect-error in global browser scope
+      return NanoCurrency.computeWork(
+        'b9cb6b51b8eb869af085c4c03e7dc539943d0bdde13b21436b687c9c7ea56cb0'
+      )
+    })
+    expect(foundWork).toBe('0000000000010600')
   })
 })

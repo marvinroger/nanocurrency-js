afe4c49998c75be1c8871ca50c7555a98061801a
Marvin ROGER <marvinroger@users.noreply.github.com>
2021-12-13 22:05:28 +0100

    Use esm for binaryen

--- packages/nanocurrency/src/wasm/scripts/optimize.mjs
+++ packages/nanocurrency/src/wasm/scripts/optimize.mjs
@@ -1,7 +1,12 @@
 /* eslint-disable */
-const fs = require('fs')
-const path = require('path')
-const binaryen = require('binaryen')
+import { fileURLToPath } from 'node:url'
+import fs from 'node:fs'
+import path from 'node:path'
+
+import binaryen from 'binaryen'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const WASM_PATH = path.resolve(__dirname, '../lib/nanocurrency.wasm')
 
